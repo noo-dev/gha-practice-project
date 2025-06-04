a32e5aa29b3e15b11b5c7df61ba3a2a5d43e95b2
noo-dev <noo.dev23@gmail.com>
2025-06-04 19:02:37 +0500

    added comments [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -3,7 +3,10 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
+
 // My tests
+// Add yet another comment
+
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
